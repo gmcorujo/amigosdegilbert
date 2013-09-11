75fce84ee3dc831d48b2ca5fd20e07a4add262dc
Corujo Gustavo Martín <v8martincoru@gmail.com>
2013-09-11 20:53:17 -0300

    creacion de apps y el panel admin

--- amigosdegilbert/settings.py
+++ amigosdegilbert/settings.py
@@ -1,5 +1,5 @@
 # Django settings for amigosdegilbert project.
-
+import os
 from local_settings import *
 
 DEBUG = True
@@ -41,7 +41,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.normpath(os.path.join(os.path.dirname(__file__),'media/'))
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -52,7 +52,7 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = '/media/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -109,10 +109,9 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    # 'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    # 'django.contrib.admindocs',
+    'django.contrib.admin',
+    'django.contrib.admindocs',
+    'amigosdegilbert.apps',
 )
 
 # A sample logging configuration. The only tangible logging
