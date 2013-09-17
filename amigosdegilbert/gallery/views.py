befbbd6e8bd140b1d63e9e99120354e439002ded
richar <ricardosiri68@gmail.com>
2013-09-16 22:19:44 -0300

    galeria con imagenes

--- amigosdegilbert/gallery/views.py
+++ amigosdegilbert/gallery/views.py
@@ -1,5 +1,22 @@
 from amigosdegilbert.template import BaseTemplate
+import os
+import Image
+
+srcPath = "amigosdegilbert/static/media/"
+def sortImages(x,y):
+	x = Image.open(srcPath+x).size[1]
+	y = Image.open(srcPath+y).size[1]
+
+	return -1 if x > y else 0 if x == y else 1
+
+def getImages():
+	images = os.listdir(srcPath)
+	images.sort(sortImages)
+	return images
 
 def index(request):
-	tempBase = BaseTemplate(request, 'gallery/index.html',{})
+	
+	tempBase = BaseTemplate(request, 'gallery/index.html',
+		{"panels":xrange(5),
+		"images":getImages()})
 	return tempBase.render()
--- amigosdegilbert/template.py
+++ amigosdegilbert/template.py
@@ -5,6 +5,6 @@ class BaseTemplate(TemplateResponse):
 	
 	def __init__(self, request, template, context=None, mimetype=None, 
 		status=None, content_type=None, current_app=None):
-		super(BaseTemplate, self).__init__(request, template, context=None, mimetype=None, status=None, content_type=None, current_app=None)
+		super(BaseTemplate, self).__init__(request, template, context, mimetype=None, status=None, content_type=None, current_app=None)
 
 
