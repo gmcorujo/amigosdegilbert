c18704a10e94d5a3cb26aacd2f99edf9cb78565a
richar <ricardosiri68@gmail.com>
2013-09-18 13:04:35 -0300

    depurado del responsive del menu a 1024px

--- amigosdegilbert/static/js/base-foot.js
+++ amigosdegilbert/static/js/base-foot.js
@@ -1,12 +1,28 @@
 (function(){
+		// buscador focus behavior
 		var buscador = document.getElementById("buscador");
 		var content = document.getElementsByClassName("content")[0];
 		buscador.onfocus = function(evt){
 			content.style.marginTop = "110px";
 		};
 		buscador.onblur = function(evt){
-			if (content.style.marginTop == "110px"){
-				content.style.marginTop = "60px";
-			}
+			content.style.marginTop = content.style.marginTop == "110" ? "60px";
 		};
+
+		// navvar hover hover behavior
+		$(".header .navbar-nav a").hover(
+			function(evt){
+				var innerText = this.childNodes[1];
+				var display = document.defaultView.getComputedStyle(innerText).getPropertyValue("display");
+				if(display != "inline"){
+					innerText.style.display = "inline";
+				}
+			},
+			function(evt){
+				var innerText = this.childNodes[1];
+				if(innerText.style.display == "inline"){
+					this.childNodes[1].style.display = "none";
+				}
+			}
+		);
 })();
